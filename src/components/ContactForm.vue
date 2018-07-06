416ad1b7769aac33fbf6ed4cb7ab5dc2709ea1d0
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-07-06 07:58:49 -0400

    mobile contact page

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,82 @@
+<template>
+  <form @submit.prevent="validate">
+    <div class="field">
+      <label for="name">Full Name *</label>
+      <input type="text" v-model="name">
+    </div>
+    <div class="field">
+      <label for="email">Email Address *</label>
+      <input type="email" v-model="email">
+    </div>
+    <div class="field">
+      <label for="phone">Phone Number</label>
+      <input type="tel" v-model="phone">
+    </div>
+    <div class="field">
+      <label for="message">Write Your Request</label>
+      <textarea v-model="message"></textarea>
+    </div>
+    <button type="submit">Send Now</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ContactForm',
+  data() {
+    return {
+      contact: {
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
+      },
+    };
+  },
+  methods: {
+    validate() {
+      return false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/app.scss";
+.field {
+  width: 100%;
+  position: relative;
+}
+input,
+textarea {
+  width: 100%;
+  border: solid $gray;
+  border-width: 0 0 1px 0;
+  padding: 10px;
+  margin-bottom: 15px;
+}
+input {
+  height: 46px;
+}
+textarea {
+  height: 138px;
+}
+label {
+  position: absolute;
+  left: 10px;
+  top: 10px;
+  font-size: 14px;
+  color: lightgray;
+  pointer-events: none;
+}
+button {
+  background: none;
+  outline: none;
+  border: none;
+  width: 100%;
+  font-weight: bold;
+  text-transform: uppercase;
+  font-size: 14px;
+}
+</style>
+
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,12 +1,65 @@
 <template>
-  <div class="contact"></div>
+  <div class="container">
+    <div class="about">
+      <h2>About</h2>
+      <img src="https://placebear.com/800/800" alt="">
+      <p>
+        I've always felt a strong pull towards different forms of media, especially
+        those that featured unique illustrative work. In pursuit of my desire to affect
+        the same media I grew up loving, I set out studying classical illustrative
+        techniques at the Birmingham-Bloomfield Art Center, and later the College for
+        Creative Studies. It was here that I was exposed to graphic design and the
+        possibilities of merging traditional techniques with today's technologies.
+      </p>
+      <div class="methods">
+        <a href="mailto:austinkandt@gmail.com" target="_blank">austinkandt@gmail.com</a>
+        <a href="tel:+12483027435">(248) 302-7435</a>
+      </div>
+    </div>
+    <div class="contact">
+      <h2>Contact</h2>
+      <contact-form></contact-form>
+    </div>
+  </div>
 </template>
 
 <script>
+import ContactForm from '../components/ContactForm.vue';
+
 export default {
   name: 'Contact',
   data() {
     return {};
   },
+  components: {
+    'contact-form': ContactForm,
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.about {
+  img {
+    $size: 220px;
+    display: block;
+    width: $size;
+    height: $size;
+    border-radius: $size;
+    object-fit: cover;
+    margin: 0 auto;
+  }
+  a {
+    font-weight: bold;
+    display: block;
+    font-size: 16px;
+    margin-bottom: 20px;
+  }
+}
+.contact {
+  margin-top: 40px;
+  h2 {
+    text-align: center;
+  }
+}
+</style>
+
