5891eba3980f740b8e5345e3e620a72bd54efab3
Britta Rodenbeck <cheers@brittarodenbeck.com>
2019-04-26 21:22:31 -0400

    :construction: redesign - scaffolding out contact/about page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="contact">
-    <contact-form />
+    <p>
+      User interface and graphic designer focused on the proliferation of functional design in the
+      tech world.
+      <br>
+      Happily working and accepting freelance gigs as a designer at my LLC, i kandt design.
+    </p>
+    <p>
+      BFA in illustration from the College for Creative Studies.
+      <br>
+      I frequently travel internationaly, most recently &mdash; New Zealand.
+    </p>
+    <router-link to="/projects">
+      <a>See my work</a>
+    </router-link>
+    <section class="Contact">
+      <p>Oh look, a message box. You know what to do.</p>
+      <contact-form />
+    </section>
   </div>
 </template>
 
