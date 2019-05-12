f139951d86158ddc887beb7631b4ef6a69b22d4e
Britta Rodenbeck <cheers@brittarodenbeck.com>
2019-05-12 16:22:41 -0400

    :lipstick: redesign - styling navigation menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,50 +1,110 @@
 <template>
-  <nav>
-    <ul>
-      <span class="Nav__left">
-        <li>
-          <a href="https://www.linkedin.com/in/austinkandt/" target="_blank">
-            <img src="https://placekitten.com/20" alt="LinkedIn logo" />
-          </a>
-        </li>
-        <li>
-          <a href="https://dribbble.com/austinkandt" target="_blank">
-            <img src="https://placekitten.com/20" alt="Dribbble logo" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/austinkandt/" target="_blank">
-            <img src="https://placekitten.com/20" alt="Instagram logo" />
-          </a>
-        </li>
-      </span>
-      <span class="Nav__right">
-        <li>
-          <router-link to="/projects">
-            <a>Work</a>
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/contact">
-            <a>Info</a>
-          </router-link>
-        </li>
-      </span>
-    </ul>
+  <nav class="navigation">
+    <span class="wrapper">
+      <ul class="navigation__items">
+        <span class="navigation__items--left">
+          <li class="navigation__item m-right-20">
+            <a href="https://www.linkedin.com/in/austinkandt/" target="_blank">
+              <linkedin-icon />
+            </a>
+          </li>
+          <li class="navigation__item m-right-20">
+            <a href="https://dribbble.com/austinkandt" target="_blank">
+              <dribbble-icon />
+            </a>
+          </li>
+          <li class="navigation__item m-right-20">
+            <a href="https://www.instagram.com/austinkandt/" target="_blank">
+              <instagram-icon />
+            </a>
+          </li>
+          <li class="navigation__logo">
+            <router-link to="/">
+              <a>i kandt . design</a>
+            </router-link>
+          </li>
+        </span>
+        <span class="navigation__items--right">
+          <li class="navigation__item">
+            <router-link to="/projects">
+              <a>Work</a>
+            </router-link>
+          </li>
+          <li class="navigation__item m-left-20">
+            <router-link to="/contact">
+              <a>Info</a>
+            </router-link>
+          </li>
+        </span>
+      </ul>
+    </span>
   </nav>
 </template>
 
 <script>
+import DribbbleIcon from './icons/Dribbble.vue';
+import InstagramIcon from './icons/Instagram.vue';
+import LinkedInIcon from './icons/LinkedIn.vue';
+
 export default {
   name: 'Navigation',
   data() {
     return {};
   },
+  components: {
+    'dribbble-icon': DribbbleIcon,
+    'instagram-icon': InstagramIcon,
+    'linkedin-icon': LinkedInIcon,
+  },
 };
 </script>
 
 <style>
-ul {
+.navigation {
+ padding: 90px 0;
+}
+
+.navigation__items {
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  list-style-type: none;
   margin: 0;
+  padding: 0;
+}
+
+.navigation__items--left {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.navigation__items--right {
+  display: none;
+}
+
+.navigation__item {
+  display: flex;
+  align-items: center;
+}
+
+.navigation__logo {
+  font-size: 26px;
+  font-weight: bold;
+  width: 100%;
+}
+
+@media only screen and (min-width: 768px) {
+  .navigation {
+    padding: 110px 0;
+  }
+
+  .navigation__items--right {
+    display: flex;
+  }
+
+  .navigation__logo {
+    width: auto;
+  }
 }
 </style>
