509bfece3461f11870f57c03ae7b42ccacd6fff4
Britta Rodenbeck <cheers@brittarodenbeck.com>
2019-05-28 11:24:24 -0400

    :lipstick: redesign - desktop styles for projects index page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,7 +2,7 @@
   <div class="projects">
     <div class="wrapper">
       <h1><strong>Work</strong><em> &mdash; Selected projects from 2016 - 2019</em></h1>
-      <p class="project__description m-top-10 m-bottom-75">
+      <p class="project__description project__description--header m-top-10 m-bottom-75">
         Here you’ll see a wide range of project types and industries that I’ve had the pleasure to
         work for over the recent years. You can keep track of more day to day stuff on my dribbble
         page.
@@ -13,18 +13,20 @@
             <img class="project__image" src="/images/feature__amtech.jpg" alt="Amtech" />
           </a>
         </router-link>
-        <h2 class="is-marginless">
-          <router-link to="/projects/amtech">
-            <a>
-              Amtech
-            </a>
-          </router-link>
-        </h2>
-        <p class="project__type"><em>&mdash; Branding, graphic design, web illustration</em></p>
-        <p class="project__description">
-          The manufacturing space is known to be a little slower when it comes to viewing high-end
-          design solutions as a potential advantage. Luckily, Amtech saw the opportunties.
-        </p>
+        <div>
+          <h2 class="is-marginless">
+            <router-link to="/projects/amtech">
+              <a>
+                Amtech
+              </a>
+            </router-link>
+          </h2>
+          <p class="project__type"><em>&mdash; Branding, graphic design, web illustration</em></p>
+          <p class="project__description">
+            The manufacturing space is known to be a little slower when it comes to viewing high-end
+            design solutions as a potential advantage. Luckily, Amtech saw the opportunties.
+          </p>
+        </div>
       </section>
       <section class="project">
         <router-link to="/projects/daily-ui">
@@ -32,18 +34,20 @@
             <img class="project__image" src="/images/feature__daily-ui.jpg" alt="Daily UI" />
           </a>
         </router-link>
-        <h2 class="is-marginless">
-          <router-link to="/projects/daily-ui">
-            <a>
-              Daily UI
-            </a>
-          </router-link>
-        </h2>
-        <p class="project__type"><em>&mdash; User interface design, graphic design</em></p>
-        <p class="project__description">
-          Daily UI is a 100 day design challenge with the goal of pushing your skills and portfolio
-          in user interface and product design. Here is a collection of my daily work.
-        </p>
+        <div>
+          <h2 class="is-marginless">
+            <router-link to="/projects/daily-ui">
+              <a>
+                Daily UI
+              </a>
+            </router-link>
+          </h2>
+          <p class="project__type"><em>&mdash; User interface design, graphic design</em></p>
+          <p class="project__description">
+            Daily UI is a 100 day design challenge with the goal of pushing your skills and
+            portfolio in user interface and product design. Here is a collection of my daily work.
+          </p>
+        </div>
       </section>
       <section class="project">
         <router-link to="/projects/downtown-detroit-partnership">
@@ -55,18 +59,20 @@
             />
           </a>
         </router-link>
-        <h2 class="is-marginless">
-          <router-link to="/projects/downtown-detroit-partnership">
-            <a>
-              Downtown Detroit Partnership
-            </a>
-          </router-link>
-        </h2>
-        <p class="project__type"><em>&mdash; Graphic design, web illustration</em></p>
-        <p class="project__description">
-          DDP is commited in their mission of growing Detroit’s urban core. I worked closely with
-          their team developing a wide range of designs for print, events, and functional use.
-        </p>
+        <div>
+          <h2 class="is-marginless">
+            <router-link to="/projects/downtown-detroit-partnership">
+              <a>
+                Downtown Detroit Partnership
+              </a>
+            </router-link>
+          </h2>
+          <p class="project__type"><em>&mdash; Graphic design, web illustration</em></p>
+          <p class="project__description">
+            DDP is commited in their mission of growing Detroit’s urban core. I worked closely with
+            their team developing a wide range of designs for print, events, and functional use.
+          </p>
+        </div>
       </section>
       <section class="project">
         <router-link to="/projects/gaddis-gaming">
@@ -78,19 +84,21 @@
             />
           </a>
         </router-link>
-        <h2 class="is-marginless">
-          <router-link to="/projects/gaddis-gaming">
-            <a>
-              Gaddis Gaming
-            </a>
-          </router-link>
-        </h2>
-        <p class="project__type"><em>&mdash; Art direction, branding, graphic design</em></p>
-        <p class="project__description">
-          Gaddis Gaming was looking for an influx of funding from Kickstarter to take their table
-          topper to the next level, and needed high level branding and design to stand out from the
-          crowd.
-        </p>
+        <div>
+          <h2 class="is-marginless">
+            <router-link to="/projects/gaddis-gaming">
+              <a>
+                Gaddis Gaming
+              </a>
+            </router-link>
+          </h2>
+          <p class="project__type"><em>&mdash; Art direction, branding, graphic design</em></p>
+          <p class="project__description">
+            Gaddis Gaming was looking for an influx of funding from Kickstarter to take their table
+            topper to the next level, and needed high level branding and design to stand out from
+            the crowd.
+          </p>
+        </div>
       </section>
       <section class="project">
         <router-link to="/projects/hack-wsu">
@@ -98,18 +106,21 @@
             <img class="project__image" src="/images/feature__hack-wsu.jpg" alt="Hack WSU" />
           </a>
         </router-link>
-        <h2 class="is-marginless">
-          <router-link to="/projects/hack-wsu">
-            <a>
-              Hack WSU
-            </a>
-          </router-link>
-        </h2>
-        <p class="project__type"><em>&mdash; Graphic design, web illustration</em></p>
-        <p class="project__description">
-          Wayne State University was holding their yearly hackathon, and they were looking to take a
-          major step up from previous years attendance numbers with a unique design concept.
-        </p>
+        <div>
+          <h2 class="is-marginless">
+            <router-link to="/projects/hack-wsu">
+              <a>
+                Hack WSU
+              </a>
+            </router-link>
+          </h2>
+          <p class="project__type"><em>&mdash; Graphic design, web illustration</em></p>
+          <p class="project__description">
+            Wayne State University was holding their yearly hackathon, and they were looking to
+            take a major step up from previous years attendance numbers with a unique design
+            concept.
+          </p>
+        </div>
       </section>
       <section class="project">
         <router-link to="/projects/safewhistle">
@@ -117,18 +128,20 @@
             <img class="project__image" src="/images/feature__safewhistle.jpg" alt="SafeWhistle" />
           </a>
         </router-link>
-        <h2 class="is-marginless">
-          <router-link to="/projects/safewhistle">
-            <a>
-              Safewhistle
-            </a>
-          </router-link>
-        </h2>
-        <p class="project__type"><em>&mdash; Branding, graphic design, product design</em></p>
-        <p class="project__description">
-          An anonymous whistleblowing platform for corporations. Safewhistle provided a unique
-          challenge in implementing a cohesive brand and product at every level.
-        </p>
+        <div>
+          <h2 class="is-marginless">
+            <router-link to="/projects/safewhistle">
+              <a>
+                Safewhistle
+              </a>
+            </router-link>
+          </h2>
+          <p class="project__type"><em>&mdash; Branding, graphic design, product design</em></p>
+          <p class="project__description">
+            An anonymous whistleblowing platform for corporations. Safewhistle provided a unique
+            challenge in implementing a cohesive brand and product at every level.
+          </p>
+        </div>
       </section>
     </div>
     <dribbble-posts />
@@ -175,5 +188,31 @@ h2 {
   font-size: 16px;
   line-height: 1.375;
   margin-top: 10px;
+}
+
+@media only screen and (min-width: 768px) {
+  .project__description--header {
+    max-width: 504px;
+    margin-bottom: 253px;
+  }
+
+  .project {
+    align-items: center;
+    display: flex;
+    margin-bottom: 40px;
+  }
+
+  .project:last-of-type {
+    margin-bottom: 112px;
+  }
+
+  .project__image {
+    margin: 0 40px 0 0;
+    width: 300px;
+  }
+
+  .project > div {
+    flex: 1;
+  }
 }
 </style>
