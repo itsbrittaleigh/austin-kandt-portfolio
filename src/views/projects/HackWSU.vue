25c6dbe31d7429b633e3cca5866c8d727a878d65
Britta Rodenbeck <cheers@brittarodenbeck.com>
2019-06-02 10:51:12 -0400

    :lipstick: redesign - hack wsu page styles

--- src/views/projects/HackWSU.vue
+++ src/views/projects/HackWSU.vue
@@ -21,11 +21,108 @@
         </p>
       </header>
     </div>
+    <div class="wrapper">
+      <section class="project-section project-section-1 project-section--desktop-flex">
+        <img
+          alt=""
+          class="project-section__image project-section__image--full-width"
+          src="/images/hackwsu_photo1.jpg"
+        />
+        <div class="project-section__description">
+          <h2 class="project-section__heading">
+            <strong>Connecting to the "Hacker" Community</strong>
+          </h2>
+          <p class="project-section__snippet">
+            Sharing in my videogame geekdom with the rest of the community gave me instant
+            inspiration to design 8-bit illustrations for the event to help create more buzz and by
+            in from students. As a result, this was by far the highest attended Hackathon that
+            Wayne State had hosted, and a lot of cool innovations were worked on by the
+            participants.
+          </p>
+        </div>
+      </section>
+    </div>
+    <section class="project-section project-section-2">
+      <div class="wrapper">
+        <img
+          alt=""
+          class="project-section__image is-marginless"
+          src="/images/hackwsu_characters2.png"
+        />
+      </div>
+    </section>
+    <section class="project-section project-section-3 project-section--with-reverse-background">
+      <div class="wrapper">
+        <div class="row row--1">
+          <div class="is-marginless">
+            <img
+              alt=""
+              class="project-section__image project-section__image--with-shadow"
+              src="/images/hackwsu_sunsethero.jpg"
+            />
+          </div>
+        </div>
+        <div class="row row--1 row--desktop-2">
+          <div class="is-marginless">
+            <img
+              alt=""
+              class="project-section__image project-section__image--with-shadow"
+              src="/images/hackwsu_photo2.jpg"
+            />
+          </div>
+          <div class="is-marginless">
+            <img
+              alt=""
+              class="project-section__image project-section__image--with-shadow"
+              src="/images/hackwsu_photo6.jpg"
+            />
+          </div>
+        </div>
+        <div class="row row--1">
+          <div class="is-marginless">
+            <img
+              alt=""
+              class="project-section__image project-section__image--with-shadow"
+              src="/images/hackwsu_underground.jpg"
+            />
+          </div>
+        </div>
+        <div class="row row--1">
+          <div class="is-marginless">
+            <img
+              alt=""
+              class="project-section__image project-section__image--with-shadow"
+              src="/images/hackwsu_photo6.jpg"
+            />
+          </div>
+        </div>
+        <div class="row row--1 row--desktop-2">
+          <div class="is-marginless">
+            <img
+              alt=""
+              class="project-section__image project-section__image--with-shadow"
+              src="/images/hackwsu_photo5.jpg"
+            />
+          </div>
+          <div class="is-marginless">
+            <img
+              alt=""
+              class="project-section__image project-section__image--with-shadow"
+              src="/images/hackwsu_photo3.jpg"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+    <cta-section />
+    <more-projects />
   </div>
 </template>
 
 <script>
+import CTASection from '../../components/CTASection.vue';
 import HackWSUIcon from '../../components/icons/HackWSU.vue';
+import MoreProjects from '../../components/MoreProjects.vue';
 
 export default {
   name: 'HackWSU',
@@ -33,11 +130,39 @@ export default {
     return {};
   },
   components: {
+    'cta-section': CTASection,
     'hack-wsu-icon': HackWSUIcon,
+    'more-projects': MoreProjects,
   },
 };
 </script>
 
-<style>
+<style scoped>
+.project-section-1 {
+  margin-bottom: 121px;
+}
 
+.project-section-2 {
+  margin-bottom: -13px;
+}
+
+.project-section-2 .project-section__image {
+  position: relative;
+  z-index: 10;
+}
+
+.project-section-3 {
+  padding-top: 203px;
+  margin-bottom: 208px;
+}
+
+@media only screen and (min-width: 768px) {
+  .project-section-1 {
+    margin-bottom: 200px;
+  }
+
+  .project-section-2 {
+    margin-bottom: -38px;
+  }
+}
 </style>
