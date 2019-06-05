1a07c813293cb7ba2e8e72e54dec0109f008cce1
Britta Rodenbeck <cheers@brittarodenbeck.com>
2019-06-05 19:35:57 -0400

    :sparkles: redesign - form field styles and validation

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,69 +1,111 @@
 <template>
   <form method="post" @submit.prevent="handleSubmit">
     <input type="hidden" name="form-name" value="contact">
-    <p
-      v-for="(error, index) in errors"
-      :key="index"
-    >
-      {{ error }}
+    <p class="is-hidden">
+      <label>Don’t fill this out: <input name="bot-field"></label>
     </p>
     <p
-      v-if="wasSuccessful"
-      class="center"
+      v-if="formErrored"
+      class="text-center form__error"
     >
-      I have received your message and will be responding soon.
+      There was a problem submitting your form. Please try again later.
     </p>
-    <template v-else class="Fields">
-      <p class="is-hidden">
-        <label>Don’t fill this out: <input name="bot-field"></label>
-      </p>
-      <div class="Field Field--half">
-        <label
-          for="name"
-          class="Field__label"
-        >
-          Name *
-        </label>
-        <input type="text" v-model="name">
+    <section class="form__fields">
+      <div class="col-lt">
+        <div class="form__field">
+          <label
+            for="name"
+            class="form__label"
+          >
+            Name *
+            <span class="form__error" v-if="nameError">{{ nameError }}</span>
+          </label>
+          <input
+            :class="{
+              'form__input': true,
+              'form__input--error': nameError,
+            }"
+            placeholder="Austin Kandt"
+            type="text"
+            v-model="name"
+          />
+        </div>
+        <div class="form__field">
+          <label
+            for="email"
+            class="form__label"
+          >
+            Email *
+            <span class="form__error" v-if="emailError">{{ emailError }}</span>
+          </label>
+          <input
+            :class="{
+              'form__input': true,
+              'form__input--error': emailError,
+            }"
+            placeholder="austinkandt@gmail.com"
+            type="text"
+            v-model="email"
+          />
+        </div>
+        <div class="form__field">
+          <label
+            for="company"
+            class="form__label form__label--optional"
+          >
+            Company (Optional)
+          </label>
+          <input
+            class="form__input"
+            type="text"
+            v-model="company"
+            placeholder="Kandt Design, LLC"
+          />
+        </div>
+        <div class="form__field">
+          <label
+            for="title"
+            class="form__label form__label--optional"
+          >
+            Title (Optional)
+          </label>
+          <input
+            class="form__input"
+            placeholder="Founder"
+            type="text"
+            v-model="title"
+          />
+        </div>
       </div>
-      <div class="Field Field--half">
-        <label
-          for="email"
-          class="Field__label"
-        >
-          Email *
-        </label>
-        <input type="email" v-model="email">
+      <div class="col-rt">
+        <div class="form__field">
+          <label
+            for="message"
+            class="form__label"
+          >
+            Message *
+            <span class="form__error" v-if="messageError">{{ messageError }}</span>
+          </label>
+          <textarea
+            :class="{
+              'form__textarea': true,
+              'form__textarea--error': messageError,
+            }"
+            placeholder="Type message ..."
+            v-model="message"
+          />
+        </div>
       </div>
-      <div class="Field">
-        <label
-          for="company"
-          class="Field__label"
-        >
-          Company (optional)
-        </label>
-        <input type="text" v-model="company">
-      </div>
-      <div class="Field">
-        <label
-          for="title"
-          class="Field__label"
-        >
-          Title (optional)
-        </label>
-        <input type="text" v-model="title">
-      </div>
-      <div class="Field">
-        <label
-          for="message"
-          class="Field__label"
-        >
-          Message *
-        </label>
-        <textarea v-model="message" />
-      </div>
-      <button type="submit">Send Now</button>
-    </template>
+      <button
+        :class="{
+          'form__button': true,
+          'form__button--success': wasSuccessful,
+        }"
+        type="submit"
+      >
+        {{ wasSuccessful ? 'Sent!' : 'Send' }}
+      </button>
+    </section>
   </form>
 </template>
 
@@ -72,11 +114,14 @@ export default {
   name: 'ContactForm',
   data() {
     return {
-      errors: [],
       company: '',
       email: '',
+      emailError: null,
+      formErrored: false,
       message: '',
+      messageError: null,
       name: '',
+      nameError: null,
       title: '',
       wasSuccessful: false,
     };
@@ -100,30 +145,28 @@ export default {
           }),
         })
           .then(this.wasSuccessful = true)
-          .catch(error => this.errors.push(error));
+          .catch(this.formError = true);
       }
     },
     validateForm() {
-      this.errors = [];
-      if (!this.name) this.errors.push('Please provide your name.');
-      if (!this.email) this.errors.push('Please provide your email address.');
-      else if (!this.validateEmail()) this.errors.push('Please provide a valid email address.');
-      if (!this.message) this.errors.push('Please write a brief message.');
-      return (this.errors.length === 0);
+      if (!this.name) this.nameError = ' - Required';
+      else this.nameError = null;
+
+      if (!this.email) this.emailError = ' - Required';
+      else if (!this.validateEmail()) this.emailError = ' - Not a valid email';
+      else this.emailError = null;
+
+      if (!this.message) this.messageError = ' - Required';
+      else if (this.message.length < 20) this.messageError = ' - 20 character minimum';
+      else this.messageError = null;
+
+      return !this.nameError && !this.emailError && !this.messageError;
     },
     validateEmail() {
       const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
       return re.test(String(this.email).toLowerCase());
     },
   },
 };
 </script>
-
-<style scoped>
-.Field__label {
-  display: block;
-}
-.Field__input {
-  display: block;
-}
-</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,27 +1,33 @@
 <template>
   <div id="contact">
-    <p>
-      User interface and graphic designer focused on the proliferation of functional design in the
-      tech world.
-      <br>
-      Happily working and accepting freelance gigs as a designer at my LLC, i kandt design.
-    </p>
-    <p>
-      BFA in illustration from the College for Creative Studies.
-      <br>
-      I frequently travel internationaly, most recently &mdash; New Zealand.
-    </p>
-    <router-link to="/projects">
-      <a>See my work</a>
-    </router-link>
-    <section class="Contact">
-      <p>Oh look, a message box. You know what to do.</p>
-      <contact-form />
-    </section>
+    <div class="wrapper">
+      <div class="project__description">
+        <p class="project__snippet m-bottom-40">
+          Moving from painting, to graphic design, to branding, to user interfaces, to product
+          development, I thought I was safe to say I’d seen it all at this point. I’ll never stop
+          learning and adapting in this industry.
+        </p>
+        <p class="project__snippet m-bottom-40">
+          BFA in illustration from the College for Creative Studies.
+          <br>
+          I frequently travel internationaly, most recently &mdash; New Zealand.
+        </p>
+        <router-link to="/projects">
+          <a class="project__link">
+            See my work
+            <arrow-icon />
+          </a>
+        </router-link>
+      </div>
+      <section class="contact">
+        <contact-form />
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+import ArrowIcon from '../components/icons/Arrow.vue';
 import ContactForm from '../components/ContactForm.vue';
 
 export default {
@@ -30,11 +36,28 @@ export default {
     return {};
   },
   components: {
+    'arrow-icon': ArrowIcon,
     'contact-form': ContactForm,
   },
 };
 </script>
 
-<style>
+<style scoped>
+.project__description {
+  margin-top: 30px;
+}
 
+.contact {
+  margin-top: 126px;
+}
+
+@media only screen and (min-width: 768px) {
+  .project__description {
+    margin-top: 10px;
+  }
+
+  .contact {
+    margin-top: 177px;
+  }
+}
 </style>
